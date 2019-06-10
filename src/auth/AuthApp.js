da089d18bc71f5671915874f438294b1ac33d59d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-10 16:30:27 -0700

    roughed out AuthApp component

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,8 +1,47 @@
 import Component from '../shared/Component.js';
+import { auth, usersRef } from '../services/firebase.js';
+
+const ui = new firebaseui.auth.AuthUI(auth);
 
 class AuthApp extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        ui.start('#firebaseui-auth-container', {
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            signInSuccessUrl: './',
+            credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+            callbacks: {
+                signInSuccessWithAuthResult(authResult) {
+                    const user = authResult.user;
+                    usersRef.child(user.uid)
+                        .set({
+                            id: user.uid,
+                            displayName: user.displayName,
+                            photoURL: user.photoURL
+                        });
+                    return true;
+                }
+            }
+        });
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
+        <div>
+            <main>
+                <p>Get signed in to use the Nasa API app!</p>
+                <div id="firebaseui-auth-container">
+                    <!--firebase auth here -->
+                </div>
+            </main>
+        </div>
     `;
     }
 }
