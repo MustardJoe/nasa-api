58855de8e55da947193a6d4f2d509c83c0540860
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-10 22:52:11 -0700

    created header component

---
+++ src/shared/Header.js
@@ -0,0 +1,11 @@
+import Component from './Component.js';
+
+class Header extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <header>Nasa API a la Jon</header>
+    `;
+    }
+}
+
+export default Header;
